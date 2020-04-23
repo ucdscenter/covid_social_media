484329ac03498c7f12a3e0c70969d5d897ca5976
Ezra Edgerton <ezraedgerton@MacBook-Pro-68.local>
2020-04-23 08:53:34 -0500

    adding size shader, look edits

--- twitter_network/static/twitter_network/src/webgl_script.js
+++ twitter_network/static/twitter_network/src/webgl_script.js
@@ -10,16 +10,32 @@ async function wrapper(){
 	let fname = d3.select("#identifier").text() + "data.json"
 	let data =  await d3.json("/static/twitter_network/data/" + fname)
 	console.log(data)
+
+	
+
 	var vertices = [];
 	let colors = [];
 
 	let colorScheme = d3.schemeTableau10
 	let color = new THREE.Color()
-	const spreadMult = 150
+	const spreadMult = 100
 	var raycaster;
 	var threshold = 0.1;
 	var geometry = new THREE.BufferGeometry();
-	var material = new THREE.PointsMaterial( { size: 1, vertexColors: true } );
+	var material = new THREE.ShaderMaterial( {
+
+					uniforms: {
+						color: { value: new THREE.Color( 0xffffff ) },
+						pointTexture: { value: new THREE.TextureLoader().load( "/static/twitter_network/images/disc.png" ) }
+					},
+					vertexShader: document.getElementById( 'vertexshader' ).textContent,
+					fragmentShader: document.getElementById( 'fragmentshader' ).textContent,
+
+					alphaTest: 0.9
+
+		} );
+
+	//var material = new THREE.PointsMaterial({size : 1, vertexColors : true})
 	var points = new THREE.Points( geometry, material );	
  	var camera, scene, renderer;
 	var geometry, material, mesh;
@@ -45,9 +61,17 @@ async function wrapper(){
 
 		var indices = new Uint16Array( data.data.length );
 		var k = 0;
+		let max_pop = -1
 		for ( var i = 0; i < data.data.length; i ++ ){
 			indices[ i ] = i;
+			
+			if (parseInt(data.data[i].p) > max_pop){
+				max_pop = parseInt(data.data[i].p)
+			}
 		}
+
+
+		let sizeScale = d3.scaleLinear().domain([0, max_pop]).range([1, 30])
 
 		geometry.setAttribute( 'position', new THREE.Float32BufferAttribute(data.data.map(function(d){
 			var x = d.l[0] * spreadMult;
@@ -55,11 +79,22 @@ async function wrapper(){
 			var z = 0//d.l[2] * spreadMult;
 			return [x,y,z]
 		}).flat(), 3 ));
-		geometry.setAttribute( 'color', new THREE.Float32BufferAttribute(data.data.map(function(d){
+		geometry.setAttribute( 'ca', new THREE.Float32BufferAttribute(data.data.map(function(d){
 			color.setHex("0x" + colorScheme[d.c].slice(1))
 			return [color.r, color.g, color.b]
 		}).flat(), 3 ));
 		geometry.setIndex( new THREE.BufferAttribute( indices, 1 ) );
+		geometry.setAttribute('size', new THREE.Float32BufferAttribute( data.data.map(function(d){
+			
+			/*var scalesize = (d.p / max_pop) 
+			if (scalesize > 1){
+				console.log(max_pop)
+				console.log(d.p)
+				console.log(scalesize)
+			}*/
+			return [sizeScale(parseInt(d.p))];
+		}).flat(), 1))
+
 
 		geometry.computeBoundingSphere();
  
@@ -71,7 +106,7 @@ async function wrapper(){
 				Mesh: {},
 				Line: {},
 				LOD: {},
-				Points: { threshold: 1 },
+				Points: { threshold: .5 },
 				Sprite: {}
 			};
 		mouse = new THREE.Vector2();
@@ -81,12 +116,8 @@ async function wrapper(){
 	    camera.lookAt(scene.position);
 	 	scene.add(points);
 	 	loader.load("/static/twitter_network/src/helvetiker.json", function(text){
-	 		console.log(text)
 	 		let cIndex = 0
 	 		data.centroids.forEach(function(c){
-	 			console.log(cIndex)
-
-	 			
 	 			var sphereMat = new THREE.MeshBasicMaterial({
  					color: new THREE.Color().setHex("0x" + colorScheme[cIndex].slice(1)),
 					transparent: true,
@@ -99,18 +130,17 @@ async function wrapper(){
 					} );
 	 			var textGeo = new THREE.TextGeometry(c[1], {
 	 				font : text,
-	 				size: 3,
+	 				size: 1,
 	 				height: 0,
 	 				bevelThickness : 1,
 	 				bevelOffset : 1
 	 			})
-	 			var sphereGeo = new THREE.CircleGeometry(2, 32)
+
+	 			var sphereGeo = new THREE.BoxGeometry(c[1].length, 5, .1)
 	 		cIndex++;
 	 		textGeo.center()
-	 		console.log(c)
 			let centroidLabel = new THREE.Mesh(textGeo, textMat)//makeTextSprite(c[1], "rgb(255,255,255)")
 			let centroidSphere = new THREE.Mesh(sphereGeo, sphereMat)
-			console.log(c[0][0] * spreadMult)
 			centroidLabel.position.set((c[0][0] * spreadMult), c[0][1] * spreadMult , 0)
 			centroidSphere.position.set((c[0][0] * spreadMult), c[0][1] * spreadMult , 0)
 			textGroup.add(centroidLabel)
@@ -126,7 +156,7 @@ async function wrapper(){
 		//scene.add( sphere );
 	 
 	    renderer = new THREE.WebGLRenderer( { antialias: true } );
-	    renderer.setSize( window.innerWidth, window.innerHeight );
+	    renderer.setSize( window.innerWidth - 30, window.innerHeight  - 89);
 	    controls = new THREE.OrbitControls( camera, renderer.domElement );
 	    controls.enableRotate = false
 	    //controls.update();
@@ -135,8 +165,8 @@ async function wrapper(){
 	}//init
 
 	function onDocumentMouseMove( event ) {
-		mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  		mouse.y = -((event.clientY - 100) / (window.innerHeight)) * 2 + 1;
+		mouse.x = ((event.clientX )/ window.innerWidth) * 2 - 1;
+  		mouse.y = -((event.clientY  - 70) / (window.innerHeight)) * 2 + 1;
   		outermousex = event.clientX;
   		outermousey = event.clientY;
 	}
@@ -161,27 +191,26 @@ async function wrapper(){
 		if (intersects[0] != undefined){
 			//console.log(intersects[0])
 			if (intersects[0].index != index){
-				points.geometry.attributes.color.array[index * 3] = oldr
-				points.geometry.attributes.color.array[(index * 3) + 1] = oldg
-				points.geometry.attributes.color.array[(index * 3) + 2] = oldb
+				points.geometry.attributes.ca.array[index * 3] = oldr
+				points.geometry.attributes.ca.array[(index * 3) + 1] = oldg
+				points.geometry.attributes.ca.array[(index * 3) + 2] = oldb
 
 				index = intersects[ 0 ].index
-				oldr = points.geometry.attributes.color.array[index * 3]
-				oldg = points.geometry.attributes.color.array[(index * 3) + 1]
-				oldb = points.geometry.attributes.color.array[(index * 3) + 2]
-				points.geometry.attributes.color.array[index * 3] = 0
-				points.geometry.attributes.color.array[(index * 3) + 1] = 0
-				points.geometry.attributes.color.array[(index * 3) + 2] = 0
-				//console.log(points.attributes.colors[index * 3] = 1)
+				oldr = points.geometry.attributes.ca.array[index * 3]
+				oldg = points.geometry.attributes.ca.array[(index * 3) + 1]
+				oldb = points.geometry.attributes.ca.array[(index * 3) + 2]
+				points.geometry.attributes.ca.array[index * 3] = 0
+				points.geometry.attributes.ca.array[(index * 3) + 1] = 0
+				points.geometry.attributes.ca.array[(index * 3) + 2] = 0
 				fetchdoc = true
 			}
 			
 		}
 		else{
 			if (index != undefined){
-				points.geometry.attributes.color.array[index * 3] = oldr
-				points.geometry.attributes.color.array[(index * 3) + 1] = oldg
-				points.geometry.attributes.color.array[(index * 3) + 2] = oldb
+				points.geometry.attributes.ca.array[index * 3] = oldr
+				points.geometry.attributes.ca.array[(index * 3) + 1] = oldg
+				points.geometry.attributes.ca.array[(index * 3) + 2] = oldb
 			}
 			index = undefined
 			
@@ -194,7 +223,7 @@ async function wrapper(){
 				$('.obj-mouseover').addClass('hidden')
 			}
 		}
-		points.geometry.attributes.color.needsUpdate = true
+		points.geometry.attributes.ca.needsUpdate = true
   		controls.update();
 	}
 
